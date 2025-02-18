d007c123899b8f2f390762199d15875e8a03b171
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-18 15:42:30 -0300

    component input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Button from "@/components/Button";
-import { ArrowRight } from "lucide-react";
+import IconButton from "@/components/IconButton";
+import { InputField, InputIcon, InputRoot } from "@/components/Input";
+import { ArrowRight, Mail } from "lucide-react";
 
 export default function Home() {
   return (
@@ -8,6 +10,15 @@ export default function Home() {
         Enviar
         <ArrowRight />
       </Button>
+
+      <IconButton />
+
+      <InputRoot>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+        <InputField />
+      </InputRoot>
     </main>
   );
 }
